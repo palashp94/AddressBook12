9fa1efe7a6e539f56e9963320d38a02b6aa37b35
palashp94 <palashmadney@gmail.com>
2022-05-18 16:28:01 +0530

    Added:Adding Contacts To Address Book

--- src/Contacts.java
+++ src/Contacts.java
@@ -6,7 +6,71 @@ public class Contacts {
         String city;
         String zip;
         String state;
-        String phone_number;
+        String phonenumber;
         String email;
-    }
+
+        public String getFirstName() {
+                return firstname;
+        }
+
+        public void setFirstName(String firstname) {
+                this.firstname = firstname;
+        }
+
+        public String getLastName() {
+                return lastname;
+        }
+
+        public void setLastName(String lastname) {
+                this.lastname = lastname;
+        }
+
+        public String getAddress() {
+                return address;
+        }
+
+        public void setAddress(String address) {
+                this.address = address;
+        }
+
+        public String getCity() {
+                return city;
+        }
+
+        public void setCity(String city) {
+                this.city = city;
+        }
+
+        public String getZip() {
+                return zip;
+        }
+
+        public void setZip(String zip) {
+                this.zip = zip;
+        }
+
+        public String getState() {
+                return state;
+        }
+
+        public void setState(String state) {
+                this.state = state;
+        }
+
+        public String getPhoneNumber() {
+                return phonenumber;
+        }
+
+        public void setPhoneNumber(String phonenumber) {
+                this.phonenumber = phonenumber;
+        }
+
+        public String getEmail() {
+                return email;
+        }
+
+        public void setEmail(String email) {
+                this.email = email;
+        }
+}
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
-public class Main {
-        public static void main(String[] args) {
-            System.out.println("Welcome to address book program");
-        }
-    }
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        int choice;
+        AddressBook ab = new AddressBook();
+        do {
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Welcome to Address Book Program");
+            System.out.println("Kindly Enter choice \n 1. Add new Contact \n 2. Show Contact details");
+            choice = scan.nextInt();
+            switch (choice) {
+                case 1:
+                    ab.addContacts();
+                    break;
+                case 2:
+                    ab.showContacts();
+                case 3:
+                    ab.showContacts();
+                    break;
+            }
+        }
+        while (choice != 5);
+    }
+}
