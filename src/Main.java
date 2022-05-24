f40cee2b0f432e44379223019a2d889e4ddae9f2
palashp94 <palashmadney@gmail.com>
2022-05-24 14:50:07 +0530

    Added:FileIO Using In AddressBook

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,15 @@ import java.util.Scanner;
 public class Main {
     static Scanner sc = new Scanner(System.in);
 
-
     public static void main(String[] args) {
+
         System.out.println("Welcome to Address Book Program");
         Scanner scan = new Scanner(System.in);
-        //    int choice = scan.nextInt();
         int choice = 0;
         while (choice <= 8) {
 
             System.out.println("Kindly Enter choice \n 1. Add New Address Book \n 2. Edit Address Book  \n 3. Display Address Book  \n 4. Delete Address book" +
-                    " \n 5. Search Person through Name, City or State \n 6. Count contacts \n 7. Sorting \n 8. Quit");
+                    " \n 5. Search Person through Name, City or State \n 6. Count contacts \n 7. Sorting \n 8. Write data \n 9. Read data \n 10. Quit");
             choice = scan.nextInt();
             switch (choice) {
                 case 1:
@@ -37,6 +36,13 @@ public class Main {
                     AddressBook.sortByOption();
                     break;
                 case 8:
+                    FileIO fileIO = new FileIO();
+                    fileIO.writeData(AddressBook.addressBookMap);
+                    break;
+                case 9 : FileIO fileIORead = new FileIO();
+                    System.out.println(fileIORead.readData());
+                    break;
+                case 10 :
                     System.out.println("Thank you.");
                     break;
             }
